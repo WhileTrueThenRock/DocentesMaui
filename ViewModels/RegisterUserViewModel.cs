9232cdd3939afb9fdc8b7b41ebd84df81d61c0bb
Victor <jamkuko87@hotmail.com>
2024-02-04 18:50:31 +0100

    Validación de campos en registro estudiantes

--- ViewModels/RegisterUserViewModel.cs
+++ ViewModels/RegisterUserViewModel.cs
@@ -37,6 +37,17 @@ namespace EFDocenteMAUI.ViewModels
 
         [ObservableProperty]
         private ImageSource avatarImage;
+        [ObservableProperty]
+        private string _telefonoString;
+        [ObservableProperty]
+        private int _numTelefono;
+        [ObservableProperty]
+        private string _cpString;
+        [ObservableProperty]
+        private string _mensajeError;
+        
+      
+
         public RegisterUserViewModel()
         {
             IsListVisible = false;
@@ -61,7 +72,6 @@ namespace EFDocenteMAUI.ViewModels
                                                 server: APIService.GestionServerUrl);
                 ResponseModel response = await APIService.ExecuteRequest(request);
 
-                //await App.Current.MainPage.DisplayAlert("Info", response.Message, "ACEPTAR");
                 await GetUsers();
                 ShowUserInfo();
             }
@@ -81,34 +91,192 @@ namespace EFDocenteMAUI.ViewModels
                 UserList = JsonConvert.DeserializeObject<ObservableCollection<UserModel>>(response.Data.ToString());
             }
         }
+        private bool ValidateFields()
+        {
+            bool validarCampos;
+            if (!ValidateName() || !ValidateSurname() || !ValidateSurname() || !ValidateDni() || !ValidateEmail() || !ValidateTelefono() || !ValidateGrade()
+                || !ValidateStreet() || !ValidateNumber() || !ValidateCity() || !ValidateCP() || !ValidateUserName() || !ValidatePassword()      )
+            {
+                validarCampos = false;
+            }
+            else
+            {
+                validarCampos = true;
+            }
+
+            return validarCampos;
+        }
+        private bool ValidateName()
+        {
+            if (null == User.Nombre || !User.Nombre.Equals(string.IsNullOrWhiteSpace))
+            {
+                MensajeError = "El campo Nombre, no puede estar vacio";
+                return false;
+            }
+            return true;
+        }
+        private bool ValidateSurname()
+        {
+            if (null == User.Apellidos || !User.Apellidos.Equals(string.IsNullOrWhiteSpace))
+            {
+                MensajeError = "El campo Apellidos, no puede estar vacio";
+                return false;
+            }
+            return true;
+        }
+        private bool ValidateDni()
+        {
+            if (null == User.Dni || !User.Dni.Equals(string.IsNullOrWhiteSpace))
+            {
+                MensajeError = "El campo DNI no puede estar vacio";
+                return false;
+            }
+            return true;
+        }
+        private bool ValidateEmail()
+        {
+            if (null == User.Email || !User.Email.Equals(string.IsNullOrWhiteSpace))
+            {
+                MensajeError = "El campo E-mail, no puede estar vacio";
+                return false;
+            }
+            return true;
+        }
+        private bool ValidateTelefono()
+        {
+            int numericValue;
+            bool isValid = int.TryParse(TelefonoString, out numericValue)
+                           && numericValue >= 600000000
+                           && numericValue <= 999999999;
+            if (null == TelefonoString || !TelefonoString.Equals(string.IsNullOrWhiteSpace))
+            {
+                MensajeError = "El campo Teléfono, no puede estar vacio";
+                return false;
+            }else if (!isValid)
+            {
+                MensajeError = "Formato de Teléfono incorrecto";
+                return false;
+            }
+            else
+            {
+                User.Telefono = numericValue;
+            }
+            return true;
+        }
+        private bool ValidateGrade()
+        {
+            if (null == User.Curso || !User.Curso.Equals(string.IsNullOrWhiteSpace))
+            {
+                MensajeError = "El campo Curso, no puede estar vacio";
+                return false;
+            }
+            return true;
+        }
+
+        private bool ValidateStreet()
+        {
+            if (null == User.Direccion.Calle || !User.Direccion.Calle.Equals(string.IsNullOrWhiteSpace))
+            {
+                MensajeError = "El campo Calle, no puede estar vacio";
+                return false;
+            }
+            return true;
+        }
+        private bool ValidateNumber()
+        {
+            if (null == User.Direccion.Numero || !User.Direccion.Numero.Equals(string.IsNullOrWhiteSpace))
+            {
+                MensajeError = "El campo Número, no puede estar vacio";
+                return false;
+            }
+            return true;
+        }
+        private bool ValidateCity()
+        {
+            if (null == User.Direccion.Poblacion || !User.Direccion.Poblacion.Equals(string.IsNullOrWhiteSpace))
+            {
+                MensajeError = "El campo Población, no puede estar vacio";
+                return false;
+            }
+            return true;
+        }
+        private bool ValidateCP()
+        {
+            int numericValue;
+            bool isValid = int.TryParse(CpString, out numericValue)
+                           && numericValue >= 0
+                           && numericValue <= 99999;
+            if (null == CpString || !CpString.Equals(string.IsNullOrWhiteSpace))
+            {
+                MensajeError = "El campo CP, no puede estar vacio";
+                return false;
+            }else if (!isValid)
+            {
+                MensajeError = "Formato CP incorrecto";
+                return false;
+            }
+            return true;
+        }
+        private bool ValidateUserName()
+        {
+            
+            if (null == User.UserName || !User.UserName.Equals(string.IsNullOrWhiteSpace))
+            {
+                MensajeError = "El campo Nombre de Usuario,\n no puede estar vacio";
+                return false;
+            }
+            return true;
+            
+        }
+        private bool ValidatePassword()
+        {
+
+            if (null == User.Password || !User.Password.Equals(string.IsNullOrWhiteSpace))
+            {
+                MensajeError = "El campo Contraseña,\n no puede estar vacio";
+                return false;
+            }
+            return true;
+
+        }
+
         [RelayCommand]
         public async Task CreateUser()
         {
             
-            if (IsEditMode)
+            if (ValidateFields())
             {
-                UpdateUser();
+                if (IsEditMode)
+                {
+                    UpdateUser();
+                }
+                else
+                {
+                    bool okSaveImage = await UpdateImage();
+                    if (okSaveImage)
+                    {
+                        string[] fechamodificada = User.FechaNacimiento.Replace(" 0:00:00", " ").Split(' ');
+                        User.FechaNacimiento = fechamodificada[0];
+                        User.Avatar = APIService.ImagenesServerUrl + "/avatars/" + User.Id.ToString();
+                        var request = new RequestModel(method: "POST",
+                                                        route: "/auth/register",
+                                                        data: User,
+                                                        server: APIService.GestionServerUrl);
+                        ResponseModel response = await APIService.ExecuteRequest(request);
+
+                        await App.Current.MainPage.DisplayAlert("Info", response.Message, "ACEPTAR");
+                        User = new UserModel();
+                        GetUsers();
+                    }
+
+                }
             }
             else
             {
-                bool okSaveImage = await UpdateImage();
-                if(okSaveImage)
-                {
-                    string[] fechamodificada = User.FechaNacimiento.Replace(" 0:00:00", " ").Split(' ');
-                    User.FechaNacimiento = fechamodificada[0];
-                    User.Avatar = APIService.ImagenesServerUrl + "/avatars/" + User.Id.ToString();
-                    var request = new RequestModel(method: "POST",
-                                                    route: "/auth/register",
-                                                    data: User,
-                                                    server: APIService.GestionServerUrl);
-                    ResponseModel response = await APIService.ExecuteRequest(request);
-
-                    await App.Current.MainPage.DisplayAlert("Info", response.Message, "ACEPTAR");
-                    User = new UserModel();
-                    GetUsers();
-                }
-                
+                await App.Current.MainPage.DisplayAlert("Info", MensajeError, "ACEPTAR");
             }
+            
+            
             
         }
         [RelayCommand]
@@ -147,6 +315,7 @@ namespace EFDocenteMAUI.ViewModels
         {
             IsSelectedUser = true;
             IsEditMode = true;
+            IsCreateMode = false;
             IsDataEnabled = false;
             AvatarImage = User.Avatar;
         }
