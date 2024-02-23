a43e54eab6829382c9ea1c6f9fe2e6557ee2475c
dam08 <fizz.ahri@gmail.com>
2024-02-23 19:55:30 +0100

    Accordion y detalles

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -128,7 +128,6 @@ namespace EFDocenteMAUI.ViewModels
 
             };
             ImageNodeInfo.Add(fileManager);
-            GenerateSource();
             Conectar();
             ShowMainMsg();
             ImMainChat = true;
@@ -145,68 +144,6 @@ namespace EFDocenteMAUI.ViewModels
             MessageToSend += emoji;
         }
         
-        private void GenerateSource()
-        {
-            var nodeImageInfo = new ObservableCollection<FileManager>();
-
-            var calendar = new FileManager() { ItemName = "Calendario", ImageIcon = "calendar.png" };
-            var alumnos = new FileManager() { ItemName = "Alumnos", ImageIcon = "student.png" };
-            var mp3 = new FileManager() { ItemName = "Music", ImageIcon = "ricardo.jpg" };
-            var pictures = new FileManager() { ItemName = "Pictures", ImageIcon = "ricardo.jpg" };
-            var video = new FileManager() { ItemName = "Videos", ImageIcon = "ricardo.jpg" };
-
-            var pollution = new FileManager() { ItemName = "Calendar", ImageIcon = "calendar.png" };
-            var globalWarming = new FileManager() { ItemName = "Global Warming.ppt", ImageIcon = "ricardo.jpg" };
-            var sanitation = new FileManager() { ItemName = "Sanitation.docx", ImageIcon = "ricardo.jpg" };
-            var socialNetwork = new FileManager() { ItemName = "Social Network.pdf", ImageIcon = "ricardo.jpg" };
-            var youthEmpower = new FileManager() { ItemName = "Youth Empowerment.pdf", ImageIcon = "ricardo.jpg" };
-
-
-            var tutorials = new FileManager() { ItemName = "Tutorials.zip", ImageIcon = "ricardo.jpg" };
-            var typeScript = new FileManager() { ItemName = "TypeScript.7z", ImageIcon = "ricardo.jpg" };
-            var uiGuide = new FileManager() { ItemName = "UI-Guide.pdf", ImageIcon = "ricardo.jpg" };
-
-            var song = new FileManager() { ItemName = "Gouttes", ImageIcon = "ricardo.jpg" };
-
-            var camera = new FileManager() { ItemName = "Camera Roll", ImageIcon = "ricardo.jpg" };
-            var stone = new FileManager() { ItemName = "Stone.jpg", ImageIcon = "ricardo.jpg" };
-            var wind = new FileManager() { ItemName = "Wind.jpg", ImageIcon = "ricardo.jpg" };
-
-            var img0 = new FileManager() { ItemName = "WIN_20160726_094117.JPG", ImageIcon = "ricardo.jpg" };
-            var img1 = new FileManager() { ItemName = "WIN_20160726_094118.JPG", ImageIcon = "ricardo.jpg" };
-
-            var video1 = new FileManager() { ItemName = "Naturals.mp4", ImageIcon = "ricardo.jpg" };
-            var video2 = new FileManager() { ItemName = "Wild.mpeg", ImageIcon = "ricardo.jpg" };
-
-
-            mp3.SubFiles = new ObservableCollection<FileManager>
-            {
-                song
-            };
-
-            pictures.SubFiles = new ObservableCollection<FileManager>
-            {
-                camera,
-                stone,
-                wind
-            };
-            camera.SubFiles = new ObservableCollection<FileManager>
-            {
-                img0,
-                img1
-            };
-
-            video.SubFiles = new ObservableCollection<FileManager>
-            {
-                video1,
-                video2
-            };
-
-            nodeImageInfo.Add(mp3);
-            nodeImageInfo.Add(pictures);
-            nodeImageInfo.Add(video);
-            ImageNodeInfo = nodeImageInfo;
-        }
         public async Task Conectar()
         {
             ClientWebSocket = new ClientWebSocket();  // Crear una nueva instancia de ClientWebSocket.
